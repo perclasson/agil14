21a1aaee4254dd61a244df70d1b17d8b6643dd0b
Ludvig Axelsson <ludvigaxelsson@gmail.com>
2014-11-25 13:17:21 +0100

    Added first move strategy

--- src/kth/game/othello/player/movestrategy/FirstMoveStrategy.java
+++ src/kth/game/othello/player/movestrategy/FirstMoveStrategy.java
@@ -1,24 +1,17 @@
 package kth.game.othello.player.movestrategy;
 
 import java.util.List;
-import java.util.Random;
 
 import kth.game.othello.Othello;
 import kth.game.othello.board.Node;
 import kth.game.othello.move.Move;
 import kth.game.othello.move.MoveCalculator;
 
-public class MoveRandomStrategy implements MoveStrategy {
-
-	private Random random;
-
-	public MoveRandomStrategy(Random random) {
-		this.random = random;
-	}
+public class FirstMoveStrategy implements MoveStrategy {
 	
 	@Override
 	public String getName() {
-		return "move random strategy";
+		return "fist move strategy";
 	}
 
 	@Override
@@ -31,10 +24,12 @@ public class MoveRandomStrategy implements MoveStrategy {
 		// 3. Calculator needs a board
 		// 4. Board need players to be created
 		// Solution, let moveCalculator be injected here
+		//if (moveCalculator == null) {
+		//	return null;
+		//}
 		MoveCalculator moveCalculator = new MoveCalculator(othello.getBoard());
 		List<Move> moves = moveCalculator.getAllPossibleMoves(playerId);
-		Node target = moves.get(random.nextInt(moves.size())).getEndNode();
-		return target;
+		return moves.get(0).getEndNode();
 	}
 
 }
