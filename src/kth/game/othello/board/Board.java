eb8573b0c609f03df0390dbf0ed7a4f7cbf3af5a
Ludvig Axelsson <ludvigaxelsson@gmail.com>
2014-12-08 08:26:41 +0100

    Added new code

--- src/kth/game/othello/board/Board.java
+++ src/kth/game/othello/board/Board.java
@@ -10,15 +10,22 @@ import java.util.List;
 public interface Board {
 
 	/**
+	 * @return the maximal x-coordinate this board contains
+	 */
+	public int getMaxX();
+
+	/**
+	 * @return the maximal y-coordinate this board contains
+	 */
+	public int getMaxY();
+
+	/**
 	 * Returns the node with the given x- and y-coordinate
-	 *
-	 * @param x
-	 *            the x-coordinate of the node
-	 * @param y
-	 *            the y-coordinate of the node
+	 * 
+	 * @param x the x-coordinate of the node
+	 * @param y the y-coordinate of the node
 	 * @return the node with given x- and y-coordinate
-	 * @throws IllegalArgumentException
-	 *             if there is no {@link Node} having the specific x- and y-coordinate
+	 * @throws IllegalArgumentException if there is no {@link Node} having the specific x- and y-coordinate
 	 */
 	public Node getNode(int x, int y);
 
@@ -28,5 +35,14 @@ public interface Board {
 	 * @return the nodes of the board
 	 */
 	public List<Node> getNodes();
+
+	/**
+	 * Used to check if a the board contains a node with given coordinates.
+	 * 
+	 * @param x the x-coordinate
+	 * @param y the y-coordinate
+	 * @return true if the board has a node with given coordinates
+	 */
+	public boolean hasNode(int x, int y);
 
 }
