7878a9f3eedcd00407a3ff5b9ae1a3ea08ad3025
Ludvig Axelsson <ludvigaxelsson@gmail.com>
2014-12-09 15:06:39 +0100

    First move strategy

--- src/kth/game/othello/board/GameBoard.java
+++ src/kth/game/othello/board/GameBoard.java
@@ -1,6 +1,9 @@
 package kth.game.othello.board;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
@@ -8,8 +11,8 @@ import java.util.Set;
 import kth.game.othello.board.factory.NodeData;
 
 /**
- * This class represents a game board of Othello. The responsibility of the board is to gather the nodes included in the
- * game.
+ * This class represents a game board of Othello. The responsibility of the
+ * board is to gather the nodes included in the game.
  * 
  * @author Ludvig Axelsson
  * @author Per Classon
@@ -44,12 +47,55 @@ public class GameBoard implements Board {
 
 	@Override
 	public List<Node> getNodes() {
-		return new ArrayList<Node>(board.values());
+		ArrayList<Node> nodes = new ArrayList<Node>(board.values());
+		Collections.sort(nodes, new Comparator<Node>() {
+			@Override
+			public int compare(Node n1, Node n2) {
+				if (n1.getYCoordinate() == n2.getYCoordinate())
+					return n1.getXCoordinate() - n2.getXCoordinate();
+				else
+					return n1.getYCoordinate() - n2.getYCoordinate();
+			}
+		});
+		return nodes;
+	}
+
+	@Override
+	public int getMaxX() {
+		int maxX = 0;
+		for (Node n : board.values()) {
+			if (n.getXCoordinate() > maxX) {
+				maxX = n.getXCoordinate();
+			}
+		}
+		return maxX;
+	}
+
+	@Override
+	public int getMaxY() {
+		int maxY = 0;
+		for (Node n : board.values()) {
+			if (n.getYCoordinate() > maxY) {
+				maxY = n.getYCoordinate();
+			}
+		}
+		return maxY;
+
+	}
+
+	@Override
+	public boolean hasNode(int x, int y) {
+		for (Node n : board.values()) {
+			if (n.getXCoordinate() == x && n.getYCoordinate() == y) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	/**
-	 * Changes the occupant player on nodes in the board. The list of nodes given will have their occupant changed to
-	 * the given player id.
+	 * Changes the occupant player on nodes in the board. The list of nodes
+	 * given will have their occupant changed to the given player id.
 	 * 
 	 * @param nodesToBeChanged
 	 *            A list of nodes have their occupant player changed.
@@ -60,6 +106,10 @@ public class GameBoard implements Board {
 		for (Node n : nodesToBeChanged) {
 			changeOccupantOnNode(n, playerId);
 		}
+	}
+
+	private void changeOccupantOnNode(Node node, String playerId) {
+		board.get(node.getId()).setOccupantPlayerId(playerId);
 	}
 
 	/**
@@ -83,55 +133,16 @@ public class GameBoard implements Board {
 			for (int x = 0; x < max; x++) {
 				if (x < xMax && y < yMax) {
 					try {
-						sb.append(getNode(x, y) + " \t ");
+						sb.append(getNode(x, y).getOccupantPlayerId() + " \t ");
 					} catch (Exception e) {
-						sb.append(". \t ");
+						sb.append("\t");
 					}
 				}
-				sb.append(". \t ");
+				sb.append("\t");
 			}
 			sb.append(System.getProperty("line.separator"));
 		}
 		return sb.toString();
-	}
-
-	private void changeOccupantOnNode(Node node, String playerId) {
-		board.get(node.getId()).setOccupantPlayerId(playerId);
-	}
-
-	@Override
-	public int getMaxX() {
-		int maxX = 0;
-		for (Node n : board.values()) {
-			if (n.getXCoordinate() > maxX) {
-				maxX = n.getXCoordinate();
-			}
-		}
-		return maxX;
-	}
-
-	@Override
-	public int getMaxY() {
-
-		int maxY = 0;
-		for (Node n : board.values()) {
-			if (n.getYCoordinate() > maxY) {
-				maxY = n.getYCoordinate();
-			}
-		}
-		return maxY;
-
-	}
-
-	@Override
-	public boolean hasNode(int x, int y) {
-
-		for (Node n : board.values()) {
-			if (n.getXCoordinate() == x && n.getYCoordinate() == y) {
-				return true;
-			}
-		}
-		return false;
 	}
 
 }
--- src/kth/game/othello/player/movestrategy/FirstMoveStrategy.java
+++ src/kth/game/othello/player/movestrategy/FirstMoveStrategy.java
@@ -1,12 +1,16 @@
 package kth.game.othello.player.movestrategy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import kth.game.othello.board.Board;
 import kth.game.othello.board.Node;
 import kth.game.othello.rules.Rules;
 
 /**
  * A move strategy where the first move from all possible moves will be chosen.
- *
+ * The first move will be selected from the
+ * 
  * @author Ludvig Axelsson
  * @author Per Classon
  * @author Tommy Roshult
@@ -21,7 +25,12 @@ public class FirstMoveStrategy implements MoveStrategy {
 
 	@Override
 	public Node move(String playerId, Rules rules, Board board) {
-		// TODO come up with a new strategy
+		for (Node node : board.getNodes()) {
+			if (rules.getNodesToSwap(playerId, node.getId()).size() > 0) {
+				// A move was found, return it
+				return node;
+			}
+		}
 		return null;
 	}
 
