67b1edc973f237796a8c12b6da0642bdffb440d2
Per Classon <perwclasson@gmail.com>
2014-12-09 15:57:54 +0100

    CornerStrategy

---
+++ src/kth/game/othello/player/movestrategy/CornerStrategy.java
@@ -0,0 +1,49 @@
+package kth.game.othello.player.movestrategy;
+
+import java.util.List;
+
+import kth.game.othello.board.Board;
+import kth.game.othello.board.Node;
+import kth.game.othello.rules.Rules;
+
+/**
+ * A move strategy where the move that will swap most node of occupant player will be made.
+ * 
+ * @author Ludvig Axelsson
+ * @author Per Classon
+ * @author Tommy Roshult
+ */
+public class CornerStrategy implements MoveStrategy {
+
+	@Override
+	public String getName() {
+		return "corner strategy";
+	}
+
+	@Override
+	public Node move(String playerId, Rules rules, Board board) {
+		List<Node> nodes = board.getNodes();
+		double bestSwap = 0;
+
+		Node bestNode = null;
+		for (Node node : nodes) {
+
+			List<Node> swapped = rules.getNodesToSwap(playerId, node.getId());
+			double swapEval = evaluateNode(node, board);
+			if (swapped.size() > 0 && swapEval > bestSwap) {
+				bestSwap = swapEval;
+				bestNode = node;
+			}
+		}
+		return bestNode;
+	}
+
+	private double evaluateNode(Node node, Board board) {
+		double x = (double) node.getXCoordinate();
+		double y = (double) node.getYCoordinate();
+		double meanX = ((double) board.getMaxX()) / 2.0d;
+		double meanY = ((double) board.getMaxY()) / 2.0d;
+
+		return Math.pow((x - meanX), 2.0d) + Math.pow((y - meanY), 2.0d);
+	}
+}
