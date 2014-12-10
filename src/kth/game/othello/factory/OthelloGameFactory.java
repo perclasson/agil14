3a786c2b65f034fb65c7fdb72bd9c4e91585b4db
Ludvig Axelsson <ludvigaxelsson@gmail.com>
2014-12-10 15:01:55 +0100

    Simplified game factory

--- src/kth/game/othello/factory/OthelloGameFactory.java
+++ src/kth/game/othello/factory/OthelloGameFactory.java
@@ -1,6 +1,7 @@
 package kth.game.othello.factory;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
@@ -19,6 +20,9 @@ import kth.game.othello.move.MoveHandler;
 import kth.game.othello.player.Player;
 import kth.game.othello.player.PlayerFactory;
 import kth.game.othello.player.PlayerHandler;
+import kth.game.othello.player.movestrategy.CornerStrategy;
+import kth.game.othello.player.movestrategy.MaxSwappedStrategy;
+import kth.game.othello.player.movestrategy.MinSwappedStrategy;
 import kth.game.othello.player.movestrategy.MoveRandomStrategy;
 import kth.game.othello.player.movestrategy.MoveStrategy;
 import kth.game.othello.rules.DefaultRules;
@@ -37,68 +41,45 @@ public class OthelloGameFactory implements OthelloFactory {
 
 	private PlayerFactory playerFactory;
 	private DirectionFactory directionFactory;
+	private List<MoveStrategy> strategies;
+	private Random random;
 
 	public OthelloGameFactory() {
+		this.random = new Random();
 		this.playerFactory = new PlayerFactory();
 		this.directionFactory = new DirectionFactory();
+		this.strategies = Arrays.asList(new MaxSwappedStrategy(), new MinSwappedStrategy(), new MoveRandomStrategy(
+				random), new CornerStrategy());
 	}
 
 	@Override
 	public Othello createComputerGame() {
-		Random random = new Random();
-
-		MoveStrategy moveStrategy = new MoveRandomStrategy(random);
-
+		MoveStrategy moveStrategy = strategies.get(random.nextInt(strategies.size()));
 		List<Player> players = new ArrayList<Player>();
 		players.add(playerFactory.createComputerPlayer("Computer 1", moveStrategy));
 		players.add(playerFactory.createComputerPlayer("Computer 2", moveStrategy));
-
-		GameBoard board = new GameBoard(new Square().getNodes(BOARD_ORDER, players));
-		Rules rules = new DefaultRules(directionFactory.getAllDirections(), board);
-		PlayerHandler playerHandler = new PlayerHandler(players, rules, random);
-		MoveHandler moveHandler = new MoveHandler(board, playerHandler, rules);
-		GameScore score = new GameScore(players, board.getNodes());
-		GameStateHandler gameStateHandler = new GameStateHandler(new Stack<GameState>(), new GameStateFactory());
-		return new Game(board, playerHandler, moveHandler, score, gameStateHandler);
+		return createGame(new Square().getNodes(BOARD_ORDER, players), players);
 	}
 
 	@Override
 	public Othello createHumanGame() {
-		Random random = new Random();
 		List<Player> players = new ArrayList<Player>();
 		players.add(playerFactory.createHumanPlayer("Player 1"));
 		players.add(playerFactory.createHumanPlayer("Player 2"));
-
-		GameBoard board = new GameBoard(new Square().getNodes(BOARD_ORDER, players));
-		Rules rules = new DefaultRules(directionFactory.getAllDirections(), board);
-		PlayerHandler playerHandler = new PlayerHandler(players, rules, random);
-		MoveHandler moveHandler = new MoveHandler(board, playerHandler, rules);
-		GameScore score = new GameScore(players, board.getNodes());
-		GameStateHandler gameStateHandler = new GameStateHandler(new Stack<GameState>(), new GameStateFactory());
-		return new Game(board, playerHandler, moveHandler, score, gameStateHandler);
+		return createGame(new Square().getNodes(BOARD_ORDER, players), players);
 	}
 
 	@Override
 	public Othello createHumanVersusComputerGame() {
-		Random random = new Random();
-
-		MoveStrategy moveStrategy = new MoveRandomStrategy(random);
+		MoveStrategy moveStrategy = strategies.get(random.nextInt(strategies.size()));
 		List<Player> players = new ArrayList<Player>();
 		players.add(playerFactory.createComputerPlayer("Computer", moveStrategy));
 		players.add(playerFactory.createHumanPlayer("Human"));
-
-		GameBoard board = new GameBoard(new Square().getNodes(BOARD_ORDER, players));
-		Rules rules = new DefaultRules(directionFactory.getAllDirections(), board);
-		PlayerHandler playerHandler = new PlayerHandler(players, rules, random);
-		MoveHandler moveHandler = new MoveHandler(board, playerHandler, rules);
-		GameScore score = new GameScore(players, board.getNodes());
-		GameStateHandler gameStateHandler = new GameStateHandler(new Stack<GameState>(), new GameStateFactory());
-		return new Game(board, playerHandler, moveHandler, score, gameStateHandler);
+		return createGame(new Square().getNodes(BOARD_ORDER, players), players);
 	}
 
 	@Override
 	public Othello createGame(Set<NodeData> nodesData, List<Player> players) {
-		Random random = new Random();
 		GameBoard board = new GameBoard(nodesData);
 		Rules rules = new DefaultRules(directionFactory.getAllDirections(), board);
 		PlayerHandler playerHandler = new PlayerHandler(players, rules, random);
