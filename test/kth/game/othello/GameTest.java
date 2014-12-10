1ef793274bcd9547df906dac6ca33dc9b03c18c1
Per Classon <perwclasson@gmail.com>
2014-12-10 17:59:01 +0100

    Change in undoTest, old test was better suited for IT-test

--- test/kth/game/othello/GameTest.java
+++ test/kth/game/othello/GameTest.java
@@ -2,8 +2,8 @@ package kth.game.othello;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
@@ -40,19 +40,6 @@ public class GameTest {
 		return board;
 	}
 
-	private void resetOccupantsOnBoard(GameBoard board) {
-		for (int x = 0; x < board.getMaxX(); x++) {
-			for (int y = 0; y < board.getMaxY(); y++) {
-				setNode(x, y, null, board);
-			}
-		}
-	}
-
-	private void setNode(int x, int y, GameBoard board) {
-		when(board.getNode(x, y).getOccupantPlayerId()).thenReturn(null);
-		when(board.getNode(x, y).isMarked()).thenReturn(false);
-	}
-
 	private void setNode(int x, int y, String playerId, GameBoard board) {
 		when(board.getNode(x, y).getOccupantPlayerId()).thenReturn(playerId);
 		when(board.getNode(x, y).isMarked()).thenReturn(true);
@@ -63,23 +50,21 @@ public class GameTest {
 		GameBoard board = mockBoard(3);
 
 		PlayerHandler playerhandler = Mockito.mock(PlayerHandler.class);
-
 		MoveHandler movehandler = Mockito.mock(MoveHandler.class);
-		when(movehandler.move()).thenCallRealMethod();
-
 		Score score = Mockito.mock(Score.class);
-
 		GameStateHandler gamestatehandler = Mockito.mock(GameStateHandler.class);
-
 		Game game = new Game(board, playerhandler, movehandler, score, gamestatehandler);
 
 		setNode(0, 0, "white", board);
 		setNode(1, 0, "black", board);
-		game.move("white", "x2y0");
 
-		assertTrue(game.getBoard().getNode(2, 0).isMarked());
+		// Verify that game was added as a game state after move
+		game.move("white", "x2y0");
+		verify(gamestatehandler).add(game);
+
+		// After undo make sure we pop GameState from GameStateHandler
 		game.undo();
-		assertFalse(game.getBoard().getNode(2, 0).isMarked());
+		verify(gamestatehandler).pop();
 
 	}
 
