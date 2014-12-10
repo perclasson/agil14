71e7f37c38da80a62d5aeb2ea5453828ad9111f6
roshult <roshult89@gmail.com>
2014-12-10 15:13:04 +0100

    test setOccupantPlayerId(- notifyobservers)

--- test/kth/game/othello/board/BoardNodeTest.java
+++ test/kth/game/othello/board/BoardNodeTest.java
@@ -1,14 +1,18 @@
 package kth.game.othello.board;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class BoardNodeTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void setOccupantPlayerIdTest() {
+		BoardNode node = new BoardNode(1, 1, "white");
+		assertEquals(node.getOccupantPlayerId(), "white");
+		node.setOccupantPlayerId("black");
+		assertEquals(node.getOccupantPlayerId(), "black");
+		node.setOccupantPlayerId("white");
+		assertEquals(node.getOccupantPlayerId(), "white");
 	}
-
 }
