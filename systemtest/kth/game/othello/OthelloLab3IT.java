07d9db5091631fc61c2c6da9880cf4bf14a01fd6
Per Classon <perwclasson@gmail.com>
2014-12-10 20:43:07 +0100

    Undo integration test

--- systemtest/kth/game/othello/OthelloLab3IT.java
+++ systemtest/kth/game/othello/OthelloLab3IT.java
@@ -1,11 +1,16 @@
 package kth.game.othello;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
+import kth.game.othello.board.Node;
+import kth.game.othello.board.factory.NodeData;
 import kth.game.othello.factory.OthelloGameFactory;
 import kth.game.othello.player.Player;
 import kth.game.othello.player.PlayerFactory;
@@ -65,4 +70,59 @@ public class OthelloLab3IT extends AbstractTest {
 		assertEquals(12, scores);
 	}
 
+	@Test
+	public void undoTest() {
+		OthelloGameFactory othelloFactory = new OthelloGameFactory();
+		Othello game = othelloFactory.createComputerGame();
+		game.start();
+
+		Map<Integer, NodeData> oldNodeData = new HashMap<Integer, NodeData>();
+		Player oldPlayerInTurn = game.getPlayerInTurn();
+
+		for (Node node : game.getBoard().getNodes()) {
+			NodeData data = new NodeData(node.getXCoordinate(), node.getYCoordinate(), node.getOccupantPlayerId());
+			oldNodeData.put(data.hashCode(), data);
+		}
+
+		// Let's make 2 moves
+		game.move();
+
+		assertTrue(oldPlayerInTurn != game.getPlayerInTurn());
+
+		game.move();
+
+		// Lets make 2 undos to get back to start
+		game.undo();
+
+		// We should not have the same board after just one undo
+		boolean notAllEquals = false;
+		for (Node node : game.getBoard().getNodes()) {
+			Integer hashCode = new NodeData(node.getXCoordinate(), node.getYCoordinate(), node.getOccupantPlayerId())
+					.hashCode();
+
+			if (oldNodeData.get(hashCode).getOccupantPlayerId() != node.getOccupantPlayerId()) {
+				notAllEquals = true;
+			}
+		}
+
+		assertTrue(notAllEquals);
+
+		// However after 2 undos we should be back at the beginning
+		game.undo();
+
+		Player newPlayerInTurn = game.getPlayerInTurn();
+		assertEquals(oldPlayerInTurn, newPlayerInTurn);
+
+		boolean allEquals = true;
+		for (Node node : game.getBoard().getNodes()) {
+			Integer hashCode = new NodeData(node.getXCoordinate(), node.getYCoordinate(), node.getOccupantPlayerId())
+					.hashCode();
+
+			if (oldNodeData.get(hashCode).getOccupantPlayerId() != node.getOccupantPlayerId()) {
+				allEquals = false;
+			}
+		}
+
+		assertTrue(allEquals);
+	}
 }
