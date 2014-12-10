b80715255f4307036d89f8a5618fdc6c2b9e428c
roshult <roshult89@gmail.com>
2014-12-10 14:48:41 +0100

    Försök på gameTest

--- test/kth/game/othello/GameTest.java
+++ test/kth/game/othello/GameTest.java
@@ -1,5 +1,7 @@
 package kth.game.othello;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -12,6 +14,7 @@ import kth.game.othello.move.MoveHandler;
 import kth.game.othello.player.PlayerHandler;
 import kth.game.othello.score.Score;
 
+import org.junit.Test;
 import org.mockito.Mockito;
 
 public class GameTest {
@@ -52,13 +55,28 @@ public class GameTest {
 		when(board.getNode(x, y).isMarked()).thenReturn(true);
 	}
 
-	public void testUndo() {
-		GameBoard board = mockBoard(5);
+	@Test
+	public void undoTest() {
+		GameBoard board = mockBoard(3);
+
 		PlayerHandler playerhandler = Mockito.mock(PlayerHandler.class);
+
 		MoveHandler movehandler = Mockito.mock(MoveHandler.class);
+		when(movehandler.move()).thenCallRealMethod();
+
 		Score score = Mockito.mock(Score.class);
+
 		GameStateHandler gamestatehandler = Mockito.mock(GameStateHandler.class);
+
 		Game game = new Game(board, playerhandler, movehandler, score, gamestatehandler);
+
+		setNode(0, 0, "white", board);
+		setNode(1, 0, "black", board);
+		game.move("white", "x2y0");
+
+		assertTrue(game.getBoard().getNode(2, 0).isMarked());
+		game.undo();
+		assertFalse(game.getBoard().getNode(2, 0).isMarked());
 
 	}
 }
